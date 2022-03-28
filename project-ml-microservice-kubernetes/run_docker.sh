8e364f907d1037b8b1f4dddc20f454f59194615e
s.shams <s.shamsi@digikala.com>
2022-03-28 18:30:39 +0430

    step 1

--- project-ml-microservice-kubernetes/run_docker.sh
+++ project-ml-microservice-kubernetes/run_docker.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
-
+ 
 ## Complete the following steps to get Docker running locally
-
+ 
 # Step 1:
 # Build image and add a descriptive tag
-
-# Step 2: 
+docker build -t prediction .
+# Step 2:
 # List docker images
-
-# Step 3: 
+docker images
+# Step 3:
 # Run flask app
+docker run -p 5000:80 prediction
